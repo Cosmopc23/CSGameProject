46df0740d64d35a1954aad5b9f54a08140b000ca
Cosmo Croft <cosmopagecroft@etoncollege.org.uk>
2024-09-13 14:27:50 +0100

    Created tilemap Added extension so that more level files can be loaded in

--- CSGameProject/Extensions/SKNode + Extensions.swift
+++ CSGameProject/Extensions/SKNode + Extensions.swift
@@ -11,6 +11,26 @@ import SpriteKit
 
 extension SKNode {
     
+    
+//    
+    class func unarchiveFromFile(file: String) -> SKNode? {
+        if let path = Bundle.main.path(forResource: file, ofType: "sks") {
+            let url = URL(fileURLWithPath: path)
+            do {
+                let sceneData = try Data(contentsOf: url, options: .mappedIfSafe)
+                let archiver = NSKeyedUnarchiver(forReadingWith: sceneData)
+                archiver.setClass(self.classForKeyedUnarchiver(), forClassName: "SKScene")
+                let scene = archiver.decodeObject(forKey: NSKeyedArchiveRootObjectKey) as! SKNode
+                archiver.finishDecoding()
+                return scene
+            } catch {
+                print(error.localizedDescription)
+                return nil
+            }
+        } else {
+            return nil
+        }
+    }
 
 // scale a node so that it is a good size proportional to the screensize. This is more effective than hardcoding a size because it can be used effectively and consistently across multiple devices. E.g. Phones and iPads
     func scale(to screenSize: CGSize, width: Bool, multiplier: CGFloat) {
